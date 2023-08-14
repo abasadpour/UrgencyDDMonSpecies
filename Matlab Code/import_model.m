61e8bde9fc4d2c149f1ee450e97a480f081dceaa
abasadpour <142227000+abasadpour@users.noreply.github.com>
2023-08-14 18:47:00 +0100

    Delete Matlab Code directory

--- Matlab Code/import_model.m
+++
@@ -1,6 +0,0 @@
-function Model = import_model(filename)
-%import_model import a python model list into MATLAB
-% filename = 'C:\Users\assad\OneDrive - Ulster University\Research projects\Hui''s paper\Real data fit\Human\all_models_sample.pkl';
-fid = py.open(filename,'rb');
-data = py.pickle.load(fid);
-Model = list2cell(data);
--- Matlab Code/order_struct.m
+++
@@ -1,8 +0,0 @@
-function sortedS = order_struct(struct, field_name)
-%order_struct order a structure array based on the field name
-% suppose struct is the struct array. field_name is the field that contains date and time.
-
-T = struct2table(struct); % convert the struct array to a table
-sortedT = sortrows(T, field_name); % sort the table by 'DOB'
-sortedS = table2struct(sortedT); % change it back to struct array if necessary
-end
